683c0572b21f3bd0460ccd07d783a2795d65551c
Andrew Taghi <aataghi13@gmail.com>
2020-11-10 13:44:43 -0600

    Added some content to home and search page

--- client-frontend/src/app/app.module.ts
+++ client-frontend/src/app/app.module.ts
@@ -1,22 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { ReservationDetailsComponent } from './components/reservation-details/reservation-details.component';
 import { ReservationListComponent } from './components/reservation-list/reservation-list.component';
 import { RoomsListComponent } from './components/rooms-list/rooms-list.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ReservationDetailsComponent,
     ReservationListComponent,
-    RoomsListComponent
+    RoomsListComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
