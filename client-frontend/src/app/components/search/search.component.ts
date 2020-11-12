028a2637cf14cc2f1c7be3ef6384c6cb5c9dd65f
Klement Omeri <klementomeri97@gmail.com>
2020-11-12 01:04:33 +0100

    add a method to make a reservation

--- client-frontend/src/app/components/search/search.component.ts
+++ client-frontend/src/app/components/search/search.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ReservationService } from 'src/app/services/reservation.service';
+
 
 @Component({
   selector: 'app-search',
@@ -11,24 +13,35 @@ export class SearchComponent implements OnInit {
   rooms: any;
   checkIn: Date;
   checkOut: Date;
-  constructor(private http: HttpClient) {
+
+
+  constructor(
+    private http: HttpClient,
+    private reservationService: ReservationService
+  ) {
 
   }
+
 
   ngOnInit() {
   }
+
 
   // The search function will run once the search button is clicked, and will access all of the data in the rooms DB.
   search() {
     this.http.get('http://localhost:3000/rooms').subscribe((response) => {
       this.rooms = response;
-    })
+    });
     console.log(this.checkIn);
     console.log(this.checkOut);
-    
   }
 
+
   reserve() {
+    const roomNumber = 1;
+    this.reservationService.reserve(roomNumber, this.checkIn, this.checkOut).subscribe(
+      response => console.log(response)
+    );
     console.log(this.checkIn);
     console.log(this.checkOut);
   }
--- client-frontend/src/app/services/reservation.service.ts
+++ client-frontend/src/app/services/reservation.service.ts
@@ -3,17 +3,28 @@ import { HttpClient } from '@angular/common/http'; //Inject HttpClient as a depe
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationService {
 
-  private reservationsURL = 'postgres://rzvpluvufnhwxn:392c9a5622ec268f75a3d76eadd45a206d13dfa39138ac531c687e6c56789509@ec2-52-71-85-210.compute-1.amazonaws.com:5432/dfh5t3jjlugdgu' //URL to database
+  private reservationsURL = 'postgres://rzvpluvufnhwxn:392c9a5622ec268f75a3d76eadd45a206d13dfa39138ac531c687e6c56789509@ec2-52-71-85-210.compute-1.amazonaws.com:5432/dfh5t3jjlugdgu'; //URL to database
 
   constructor(private http: HttpClient) { }
+
 
   // getReservations() {
   //   return this.http.get(this.reservationsURL);
   // }
-  
+
+  reserve(roomNumber, checkIn, checkOut) {
+    const body = {
+      room_number: roomNumber,
+      check_in: checkIn,
+      check_out: checkOut
+    };
+    return this.http.post('http://localhost:3000/', body);
+  }
+
 }
